963b84bba3c3c12abacd6decc700b3792b292cbe
Magdi2085 <seller@mahealthus.com>
2025-11-04 17:24:10 -0600

    fix: Use start_api.sh with PYTHONPATH instead of --app-dir

--- start_api.sh
+++ start_api.sh
@@ -41,10 +41,9 @@ if [ $? -ne 0 ]; then
   }
 fi
 
-exec "$PYTHON_BIN" -m uvicorn --app-dir /app api.main:app \
+exec "$PYTHON_BIN" -m uvicorn api.main:app \
     --host 0.0.0.0 \
     --port "$PORT" \
-    --workers "$WORKERS" \
     --log-level info \
     --proxy-headers \
     --forwarded-allow-ips '*'
